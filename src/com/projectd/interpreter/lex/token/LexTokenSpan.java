5c0ba56edee293ac323c049cda6b8eeb64f6ec08
Dariya <66779337+yadariya@users.noreply.github.com>
2022-02-05 20:53:16 +0300

    Second iteration on lexer

--- src/com/projectd/interpreter/lex/token/LexTokenSpan.java
+++ src/com/projectd/interpreter/lex/token/LexTokenSpan.java
@@ -4,12 +4,20 @@ public class LexTokenSpan {
     final int lineNum;
     final int pos;
 
-    public LexTokenSpan(int lineNum, int pos) {
+    protected LexTokenSpan(int lineNum, int pos) {
         this.lineNum = lineNum;
         this.pos = pos;
     }
 
     public static LexTokenSpan of(int lineNum, int pos) {
         return new LexTokenSpan(lineNum, pos);
+    }
+
+    @Override
+    public String toString() {
+        return "LexTokenSpan{" +
+                "lineNum=" + lineNum +
+                ", pos=" + pos +
+                '}';
     }
 }
