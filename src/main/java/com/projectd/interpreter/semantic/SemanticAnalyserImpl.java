21ce4c7364815a8a4c703ff864f3ac4122676e87
Dariya <66779337+yadariya@users.noreply.github.com>
2022-03-12 14:22:28 +0300

    Semantics analyzer

--- src/main/java/com/projectd/interpreter/semantic/SemanticAnalyserImpl.java
+++ src/main/java/com/projectd/interpreter/semantic/SemanticAnalyserImpl.java
@@ -4,14 +4,25 @@ import com.projectd.interpreter.semantic.symbol.table.StNode;
 import com.projectd.interpreter.syntax.tree.AstNode;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class SemanticAnalyserImpl implements SemanticAnalyser {
+    ArrayList<AstNode> visited = new ArrayList<>();
+    List<StNode> symbolTable = new ArrayList<>();
 
     @Override
-    public void analyse(AstNode root) {
-        // Traverse tree
-        // TODO: analyse all scopes
-        List<StNode> symbolTable = new ArrayList<>();
+    // DFS
+    public void analyse(AstNode node) {
+        visited.add(node);
+        // проверка IDENTIFIER или нет
+        // если да - добавляем в таблицу, если нет итерируем дальше
+        Iterator<AstNode> children = node.getChildren().listIterator();
+        while(children.hasNext()){
+            AstNode current = children.next();
+            if(!visited.contains(current)){
+                analyse(current);
+            }
+        }
     }
 }
